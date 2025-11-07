0d6a83f22dcf9d9ad92106113d596a689e751235
Parnandi_ Sricharan_21008490 <parnandivarun@gmail.com>
2025-11-07 14:51:03 +0530

    Updated remaining bugs

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,21 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-
   theme: {
-    extend: {
-      colors: {
-        mint: {
-          light: "#C8E6E0",
-          DEFAULT: "#38B69A",
-          dark: "#2E8B75",
-        },
-        background: {
-          light: "#F3F7F6",
-          soft: "#E6F2EF",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
